647b7d694672f699987b0a5404187253340d007f
Darren Syzling <dsyzling@gmail.com>
2025-01-10 18:01:40 +0000

    Fix key defs and add WSL Wayland copy/paste

--- emacs/.emacs.d/personal/init.el
+++ emacs/.emacs.d/personal/init.el
@@ -500,7 +500,7 @@
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "C-x j") 'avy-goto-char)
--(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c a") 'org-agenda)
 ;; define a set of basic keys to allow us simple debug navigation.
 (with-eval-after-load 'dap-mode
   (define-key dap-mode-map (kbd "<f8>") 'dap-next)
@@ -524,3 +524,21 @@
   :ensure t
   :config
   (direnv-mode))
+
+;; credit: yorickvP on Github
+;; Requires wl-clipboard - sudo apt install wl-clipboard on WSL
+;;
+(setq wl-copy-process nil)
+(defun wl-copy (text)
+  (setq wl-copy-process (make-process :name "wl-copy"
+                                      :buffer nil
+                                      :command '("wl-copy" "-f" "-n")
+                                      :connection-type 'pipe))
+  (process-send-string wl-copy-process text)
+  (process-send-eof wl-copy-process))
+(defun wl-paste ()
+  (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil ; should return nil if we're the current paste owner
+    (shell-command-to-string "wl-paste -n | tr -d \r")))
+(setq interprogram-cut-function 'wl-copy)
+(setq interprogram-paste-function 'wl-paste)
