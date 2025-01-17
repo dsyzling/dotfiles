ebe128a8f9e3e133bee9de8a110d7f90aedf7ad5
Darren Syzling <dsyzling@gmail.com>
2025-01-17 12:45:02 +0000

    Install oh-my-zsh directly using curl

--- install-wsl.sh
+++ install-wsl.sh
@@ -6,4 +6,7 @@ stow fonts
 stow xorg
 stow tmux
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 tic -x -o ~/.terminfo wsl-zsh/terminfo-24bit.src
