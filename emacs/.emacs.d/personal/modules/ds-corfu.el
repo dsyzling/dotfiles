0945ee2f9dc9f21e4407ff38c0fde0758180a2a5
Darren Syzling <dsyzling@gmail.com>
2025-04-30 12:16:59 +0100

    Suggested fix for using corfu with lsp completion
    Currently without cape lsp completion hangs for 5 or so seconds. This
    fix with the cape package was suggested so temporarily adding until
    another option is available.

--- emacs/.emacs.d/personal/modules/ds-corfu.el
+++ emacs/.emacs.d/personal/modules/ds-corfu.el
@@ -46,6 +46,33 @@
   )
 
 
+;;
+;; Currently I require this to resolve an issue with lsp completion and corfu.
+;; https://github.com/minad/cape
+;;
+(use-package cape
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative key: M-<tab>, M-p, M-+
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+  )
+  
+;; suggested by https://github.com/minad/corfu/issues/188#issuecomment-1148658471
+(advice-add #'lsp-completion-at-point :around #'cape-wrap-noninterruptible)
 
 
 ;; (use-package corfu
