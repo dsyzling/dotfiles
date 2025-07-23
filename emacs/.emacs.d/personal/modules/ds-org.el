10fcf03385cefb95d0cd06908c46d0fa767df2cf
Darren Syzling <dsyzling@gmail.com>
2025-07-23 10:09:47 +0100

    Replace custom config with org-modern

--- emacs/.emacs.d/personal/modules/ds-org.el
+++ emacs/.emacs.d/personal/modules/ds-org.el
@@ -337,34 +337,13 @@ Otherwise delegate to the default org-add-note."
 
 
 ;;
-;; Experimental
-;; Configuring a different look for orgmode documents.
-;;
+;; org-modern for a modern look to org documents
+;; 
+(use-package org-modern
+  :ensure t)
+;; apply org-modern globally
+(with-eval-after-load 'org (global-org-modern-mode))
 
-;; (font-lock-add-keywords
-;;  'org-mode
-;;  '(("^ *\\([-]\\) "
-;;     (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-(setq org-hide-emphasis-markers t
-      org-fontify-done-headline t
-      org-hide-leading-stars t
-      org-pretty-entities t
-      org-odd-levels-only t)
-
-(use-package org-bullets
-  :custom
-  (org-bullets-bullet-list '("◉" "☯" "○" "☯" "✸" "☯" "✿" "☯" "✜" "☯" "◆" "☯" "▶"))
-  (org-ellipsis "⤵")
-  :hook (org-mode . org-bullets-mode))
-
-;; Show bullets instead of dashed list items.
-(font-lock-add-keywords 'org-mode
-                        '(("^ *\\([-]\\) "
-                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-(font-lock-add-keywords 'org-mode
-                        '(("^ *\\([+]\\) "
-                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "◦"))))))
 
 (setq-default prettify-symbols-alist '(("#+BEGIN_SRC" . "†")
                                        ("#+END_SRC" . "†")
@@ -373,35 +352,6 @@ Otherwise delegate to the default org-add-note."
                                        (">=" . "≥")
                                        ("=>" . "⇨")))
 (setq prettify-symbols-unprettify-at-point 'right-edge)
-
-
-;; (let* ((variable-tuple
-;;         (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-;;               ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-;;               ((x-list-fonts "Verdana")         '(:font "Verdana"))
-;;               ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-;;               (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-;;        (base-font-color     (face-foreground 'default nil 'default))
-;;        (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-
-;;   (custom-theme-set-faces
-;;    'user
-;;    `(org-level-8 ((t (,@headline ,@variable-tuple))))
-;;    `(org-level-7 ((t (,@headline ,@variable-tuple))))
-;;    `(org-level-6 ((t (,@headline ,@variable-tuple))))
-;;    `(org-level-5 ((t (,@headline ,@variable-tuple))))
-;;    `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-;;    `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.15))))
-;;    `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.2))))
-;;    `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.25))))
-;;    `(org-document-title ((t (,@headline ,@variable-tuple :height 1.3 :underline nil))))))
-
-;; (custom-theme-set-faces
-;;  'user
-;;  '(variable-pitch ((t (:family "Source Sans Pro" :height 180 :weight light))))
-;;  '(fixed-pitch ((t ( :family "JetBrainsMono"
-;;                              :slant normal :weight normal
-;;                              :height 1.0 :width normal)))))
 
 ;;
 ;; Deft and Roam for note taking and searching
